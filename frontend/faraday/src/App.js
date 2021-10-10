0dc6325032bf4480a08169322bf25b3f3bb48f01
Zshan0 <zeeshan060801@gmail.com>
2021-10-10 14:03:33 +0530

    Updated name and logo Commit before trying to fix TopBar

--- frontend/faraday/src/App.js
+++ frontend/faraday/src/App.js
@@ -56,10 +56,10 @@ const App = () => {
         <Route exact path="/">
           <p>Hello World</p>
         </Route>
-        <Route exact path="/learning" component={Learning} />
-        <Route exact path="/contests" component={Contest} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/learning" component={Learning} />
+        <Route exact path="/contests" component={Contest} />
         <Route exact path="/page/candleplots" component={CandlePlots} />
         <Route exact path="/page/mathematics" component={Mathematics} />
       </div>
